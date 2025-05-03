2534ee1040655b7627ae7612e05b7939c9e6683c
AnuJain62 <anujain.aj021@gmail.com>
2025-05-04 00:15:59 +0530

    java

---
+++ Collections_Framework.java
@@ -0,0 +1,30 @@
+// a collection represents a group of object. java collections provide classws and interfaces for us to be 
+// able to write code quickly and efficiently
+
+// why do we nned collections :  we need collection for efficient storage and better manipulation of data in java
+
+// how are collections available : collections in java are available as classes and interfaces. following are few commonly used collections in java:
+// 1) ArrayList  2) Set -> for distinct collection  3) stack -> LIFO  4) HashMap -> for storing key-value pairs 
+
+// collection class is available in java.util package collection class also provides static methods for sorting,searching etc.
+
+
+// java collection wikipedia  -> link for hierarchy of collections
+
+
+// JDK 14 documentation -> API documentation   ->> for study of classes and packageses
+// if we not find anythink so we find inside the module java.Base
+
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class Collections_Framework {
+ public static void main(String[] args) {
+  // ArrayList 
+  // Set
+ //  TreeSet
+  }
+}
---
+++ Custom_Calculator.java
@@ -0,0 +1,20 @@
+// You have to create a custom calculator with following operations:
+// 1) addition
+// 2) subtraction
+// 3) multiplication
+// 4) division
+
+// which throws the following exceptions:
+// 1) invalid input exception  ex: 8 & 9
+// 2) cannot divide by 0 exception
+// 3) max input exception if any of the input is greater than 100000
+// 4) max multiplier reached exception -> don't allow any multiplication input to be greater than 7000
+
+
+
+
+public class Custom_Calculator {
+ public static void main(String[] args) {
+  
+ }
+}
